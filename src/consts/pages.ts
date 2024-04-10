796b14865d42298e494714ee223211384a2d6630
Andru0Gx <Andy20ogx@gmail.com>
2024-04-09 22:03:18 -0400

    Add pages.ts file with navigation links in consts directory

---
+++ src/consts/pages.ts
@@ -0,0 +1,16 @@
+export const pages = [
+    {
+        title: "Inicio",
+        url: "/",
+    },
+    {
+        title: "Contacto",
+        url: "https://wa.me/584249597302",
+        rel: "noopener noreferrer",
+        target: "_blank",
+    },
+    {
+        title: "Login",
+        url: "/",
+    },
+];
