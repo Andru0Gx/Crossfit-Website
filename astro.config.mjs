0cf5e706677103c1611d6e83cca4af99d511cfbe
Andru0Gx <Andy20ogx@gmail.com>
2024-04-29 15:42:19 -0400

    Add authentication configuration file and update package.json and astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
-
 import tailwind from "@astrojs/tailwind";
+
+import auth from "auth-astro";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel(),
-  integrations: [tailwind()]
+    output: "server",
+    adapter: vercel(),
+    integrations: [tailwind(), auth()],
 });
